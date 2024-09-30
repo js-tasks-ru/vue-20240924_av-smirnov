f1e3f34385e21450fdc536e5354986652ad3e732
Смирнов Александр Васильевич <av.smirnov@titan2.ru>
2024-09-30 14:30:57 +0300

    Create app

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,12 @@
 import { defineComponent, createApp } from 'vue'
+
+const myFirstApp = defineComponent({
+  name: 'myFirstApp',
+  setup() {
+    return { today: new Date().toLocaleDateString(undefined, { dateStyle: 'long' }) }
+  },
+  template: '<div>Сегодня {{ today }}</div>',
+})
+
+const app = createApp(myFirstApp)
+app.mount('#app')
